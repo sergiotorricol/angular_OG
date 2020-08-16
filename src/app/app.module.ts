e1ad0c0ae600c8305a212bb2ee10d54de1c8824f
sergiotorricol <sergioatorricol@gmail.com>
2020-08-16 17:05:14 -0400

    My fourth first component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
