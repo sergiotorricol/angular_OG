b24a0c79b92a3bce5801f320eb143c294f57f26e
sergiotorricol <sergioatorricol@gmail.com>
2020-08-19 01:05:26 -0400

    Update.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
+import { PersonaComponent } from './components/persona/persona.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule,
