d951f163b1fb46e21251f1cbfc6739e2befba13d
sergiotorricol <sergioatorricol@gmail.com>
2020-08-21 21:04:57 -0400

    Test Modules branch cleaned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './components/test/test.component';
 import { FormsModule } from '@angular/forms';
-import { PersonaModule } from './modules/persona/persona.module';
-import {ProductoModule} from './modules/producto/producto.module';
-import{AdminModule}from './modules/admin/admin.module';
-import{UserModule}from './modules/user/user.module';
 
-import {TestPipe }from './pipes/test.pipe';
-import {ExponentPipe }from './pipes/exponent.pipe';
 
 import { from } from 'rxjs';
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent,
-    TestPipe,
-    ExponentPipe
+    AppComponent    
+    
+
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    PersonaModule,
-    ProductoModule,
-    AdminModule,
-    UserModule
+    FormsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
