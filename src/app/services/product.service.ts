d8498c737430dd9347b6f7ea11ceafa8488a2b59
sergiotorricol <sergioatorricol@gmail.com>
2020-09-04 14:58:38 -0400

    prueba

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,37 +10,7 @@ export class ProductService {
     public getProducts(): Observable<any>  {
 
       return this.http.get('https://angular-og.firebaseio.com/products.json');
-
-      
-
-       /* return [{
-            description: '',
-            imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-            ownerId: '',
-            price: '',
-            title: ''
-          },
-          {
-            description: '',
-            imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-            ownerId: '',
-            price: '',
-            title: ''
-          },
-          {
-            description: '',
-            imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-            ownerId: '',
-            price: '',
-            title: ''
-          },
-          {
-            description: '',
-            imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-            ownerId: '',
-            price: '',
-            title: ''
-          }];*/
+       
     }
     public addProduct(product: any): Observable<any> {
       return this.http.post('https://angular-course-bcdc9.firebaseio.com/products.json', product);
@@ -53,4 +23,5 @@ export class ProductService {
       return this.http.put(`https://angular-course-bcdc9.firebaseio.com/products/${id}.json`, product);
     }
 
+    //prueba
 }
