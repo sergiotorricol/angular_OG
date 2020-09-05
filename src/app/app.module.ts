49a9d002dd3d9b646fcff4f8a7924c2a1e54674a
sergiotorricol <sergioatorricol@gmail.com>
2020-09-05 16:05:01 -0400

    blank

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { AdminComponent } from './components/admin/admin.component';
-import {AppRoutingModule} from './app-routing.module';
-import { Home1Component } from './components/home1/home1.component';
-import { Home2Component } from './components/home2/home2.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import { MatSliderModule } from "@angular/material/slider";
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    AdminComponent,
-    Home1Component,
-    Home2Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    AppRoutingModule,
-    NgbModule,
-    MatSliderModule,
-    MatIconModule,  
+    FormsModule
   ],
   providers: [
   ],
